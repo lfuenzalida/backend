dfcc7a9c5c5b027260e4075b4421f2ae5a318b18
lfuenzalida <luis.fuenzalida1992@gmail.com>
2023-09-13 18:37:28 -0300

    listo

--- views/views.py
+++ views/views.py
@@ -5,13 +5,11 @@ from django.shortcuts import render
 # Create your views here.
 def inicio (request):
     return render(request, 'views/inicio.html')
-def pagina1 (request):
-    return render(request, 'views/pagina1.html')
-def pagina2(request):
-    return render(request, 'views/pagina2.html')
-def pagina3(request):
-    return render(request, 'views/pagina3.html')
-def pagina4(request):
-    return render(request, 'views/pagina4.html')
-def pagina5(request):
-    return render(request, 'views/pagina5.html')
+def iniciales(request):
+    return render(request, 'views/iniciales.html')
+def formas(request):
+    return render(request, 'views/formas.html')
+def legendarios(request):
+    return render(request, 'views/legendarios.html')
+def paradox(request):
+    return render(request, 'views/paradox.html')
